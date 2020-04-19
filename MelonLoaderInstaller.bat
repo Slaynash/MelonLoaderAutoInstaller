d86db30a8c377ae661a13e8a1133c875f0e33672
Slaynash <slaynash@survival-machines.fr>
2020-04-19 16:54:57 +0200

    Autoremove old MelonLoader install

--- MelonLoaderInstaller.bat
+++ MelonLoaderInstaller.bat
@@ -13,20 +13,28 @@ echo ^| MelonLoader 0.1.0 AutoInstaller v1 by Slaynash ^|
 echo --------------------------------------------------
 echo:
 
+echo Cleaning up existing MelonLoader installation
+IF EXIST MelonLoader rd /S /Q MelonLoader
+IF EXIST MelonLoader winmm.dll del winmm.dll
+echo:
+
 echo Downloading 7zip...
 powershell -Command "Invoke-WebRequest https://github.com/Slaynash/MelonLoaderAutoInstaller/raw/master/7z.exe -OutFile 7z.exe"
+echo:
 
 echo Downloading MelonLoader from github...
 powershell -Command "Invoke-WebRequest https://github.com/HerpDerpinstine/MelonLoader/releases/download/v0.1.0/MelonLoader.zip -OutFile melonloader.zip"
 echo Decompressing MelonLoader...
 powershell Expand-Archive melonloader.zip -DestinationPath .
 del melonloader.zip
+echo:
 
 echo Downloading Il2CppDumper from github...
 powershell -Command "Invoke-WebRequest https://github.com/Perfare/Il2CppDumper/releases/download/v6.2.1/Il2CppDumper-v6.2.1.zip -OutFile il2cppdumper.zip"
 echo Decompressing Il2CppDumper...
 powershell Expand-Archive il2cppdumper.zip -DestinationPath il2cppdumper
 del il2cppdumper.zip
+echo:
 
 echo Downloading Il2CppAssemblyUnhollower from github...
 powershell -Command "Invoke-WebRequest https://github.com/knah/Il2CppAssemblyUnhollower/releases/download/v0.2.0.0/Il2CppAssemblyUnhollower.0.2.0.0.7z -OutFile il2cppassemblyunhallower.zip"
@@ -39,6 +47,7 @@ for /f %%i in ('dir /b *_Data') do set data_folder=%%i
 
 echo Downloading Il2CppDumper config...
 powershell -Command "Invoke-WebRequest https://github.com/Slaynash/MelonLoaderAutoInstaller/raw/master/il2cppdumper_config.json -OutFile il2cppdumper/config.json"
+echo:
 
 echo Running Il2CppDumper
 cd il2cppdumper
@@ -61,5 +70,6 @@ rd /S /Q il2cppdumper
 rd /S /Q il2cppdumper_output
 rd /S /Q il2cppassemblyunhallower
 rd /S /Q il2cppassemblyunhallower_output
+echo:
 
 pause
